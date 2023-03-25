9c84a9c77a79218a886485e0ad32e54af456e4e0
hupingsheng <hu942658636@163.com>
2023-03-25 14:14:33 +0800

    服务端创建地图

--- backend/src/main/java/com/hps/backend/consumer/WebSocketServer.java
+++ backend/src/main/java/com/hps/backend/consumer/WebSocketServer.java
@@ -1,5 +1,7 @@
 package com.hps.backend.consumer;
 
+import com.alibaba.fastjson2.JSONObject;
+import com.hps.backend.consumer.utils.Game;
 import com.hps.backend.consumer.utils.JwtAuthentication;
 import com.hps.backend.mapper.UserMapper;
 import com.hps.backend.pojo.User;
@@ -10,14 +12,19 @@ import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 @Component
-@ServerEndpoint("/websocket/{token}")  // 注意不要以'/'结尾  作用：将目前的类定义成一个websocket服务器端,
+@ServerEndpoint("/websocket/{token}")  // 注意不要以'/'结尾  作用：将目前的类定义成一个websocket服务器端,类似于controller
 public class WebSocketServer {
 
     //一个静态变量线程安全map，用来存放每个客户端对应的WebSocket对象，都能看到，设计为线程安全的  <userId,websocket>
-    private static ConcurrentHashMap<Integer, WebSocketServer> users = new ConcurrentHashMap<>();
+    final private static ConcurrentHashMap<Integer, WebSocketServer> users = new ConcurrentHashMap<>();
+
+    //匹配池
+    final private static CopyOnWriteArraySet<User> matchpool = new CopyOnWriteArraySet<>();
 
     private User user;
 
@@ -26,6 +33,7 @@ public class WebSocketServer {
     private Session session = null;
 
     private static UserMapper userMapper;
+
 
     @Autowired
     public void setUserMapper(UserMapper userMapper) {
@@ -40,9 +48,9 @@ public class WebSocketServer {
         Integer userId = JwtAuthentication.getUserId(token);
         this.user = userMapper.selectById(userId);
         //用户存在，则加入连接管理，否则连接关闭
-        if(this.user != null){
+        if (this.user != null) {
             users.put(userId, this);
-        }else {
+        } else {
             this.session.close();
         }
 
@@ -55,14 +63,62 @@ public class WebSocketServer {
         System.out.println("=====closed======");
         if (this.user != null) {
             users.remove(this.user.getId());
+            matchpool.remove(this.user);
+
         }
+    }
+
+    private void startMatching() {
+        System.out.println("start matching!");
+        matchpool.add(this.user);
+
+        while (matchpool.size() >= 2){
+            Iterator<User> it = matchpool.iterator();
+            User a = it.next(), b = it.next();
+            matchpool.remove(a);
+            matchpool.remove(b);
+
+            //初始化地图
+            Game game = new Game(13, 14, 20);
+            game.createMap();
+
+            //a发送消息回去，包含对手b的信息
+            JSONObject respA = new JSONObject();
+            respA.put("event","start-matching");
+            respA.put("opponent_username",b.getUsername());
+            respA.put("opponent_photo",b.getPhoto());
+            respA.put("gamemap",game.getG());
+            users.get(a.getId()).sendMessage(respA.toJSONString());
+
+            //b发送消息给客户端，包含对手a的信息
+            JSONObject respB = new JSONObject();
+            respB.put("event","start-matching");
+            respB.put("opponent_username",a.getUsername());
+            respB.put("opponent_photo",a.getPhoto());
+            respB.put("gamemap",game.getG());
+            users.get(b.getId()).sendMessage(respB.toJSONString());
+
+
+        }
+    }
+
+    private void stopMatching() {
+        System.out.println("stop matching!");
+        matchpool.remove(this.user);
     }
 
     @OnMessage
     public void onMessage(String message, Session session) {
         // 从Client接收消息
         System.out.println("=======received message=======");
-        System.out.println("message: " + message);
+        JSONObject data = JSONObject.parseObject(message);
+        //前端发起websocket连接有两种情形：开始匹配、取消匹配  用event字段来区分
+        String event = data.getString("event");
+        if ("start-matching".equals(event)) {
+            startMatching();
+        } else if ("stop-matching".equals(event)) {
+            stopMatching();
+        }
     }
 
     @OnError
---
+++ backend/src/main/java/com/hps/backend/consumer/utils/Game.java
@@ -0,0 +1,98 @@
+package com.hps.backend.consumer.utils;
+
+import java.util.Random;
+
+public class Game {
+
+    final private Integer rows, cols, inner_walls_count;    //行数，列数，墙的数量
+
+    final private int[][] g;  //地图的每个格子  0表示空地， 1表示障碍物
+
+    final private static int[] dx = {-1, 0, 1, 0}, dy = {0, 1, 0, -1};
+
+    public Game(Integer rows, Integer cols, Integer inner_walls_count) {
+        this.rows = rows;
+        this.cols = cols;
+        this.inner_walls_count = inner_walls_count;
+        this.g = new int[rows][cols];
+    }
+
+    public int[][] getG() {
+        return g;
+    }
+
+    /**
+     *  [sx, sy]:起点  [tx, ty]:终点  判断连通性
+     * @param sx
+     * @param sy
+     * @param tx
+     * @param ty
+     * @return
+     */
+    public boolean check_connectivity(int sx, int sy, int tx, int ty) {
+        if (sx == tx && sy == ty) return true;
+        g[sx][sy] = 1;
+
+        for (int i = 0; i < 4; i++){
+            int x = sx + dx[i], y = sy + dy[i];
+            if(x >= 0 && x < this.rows && y >= 0 && y <= this.cols && g[x][y] == 0){
+                if(check_connectivity(x, y, tx, ty)){
+                    g[sx][sy] = 0;   //恢复现场
+                    return true;
+                }
+            }
+        }
+        g[sx][sy] = 0;
+        return false;
+    }
+
+    /**
+     * 构造一个满足连通性的地图
+     * @return
+     */
+    private boolean draw() {         //画地图
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < this.cols; j++) {
+                g[i][j] = 0;
+            }
+        }
+
+        //给四周加上障碍物
+        for (int r = 0; r < this.rows; r++) {
+            g[r][0] = g[r][this.cols - 1] = 1;
+        }
+        for (int c = 0; c < this.cols; c++) {
+            g[0][c] = g[this.rows - 1][c] = 1;
+        }
+
+        Random random = new Random();
+        for (int i = 0; i < this.inner_walls_count / 2; i++) {
+            for (int j = 0; j < 1000; j++) {
+                int r = random.nextInt(this.rows);
+                int c = random.nextInt(this.cols);
+
+                if (g[r][c] == 1 || g[this.rows - 1 - r][this.cols - 1 - c] == 1) continue;
+                if (r == this.rows - 2 && c == 1 || r == 1 && c == this.cols - 2) continue;
+
+                g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = 1;
+                break;
+            }
+        }
+
+        return check_connectivity(this.rows - 2,1, 1, this.cols - 2);
+
+    }
+
+    /**
+     * 1000次创建地图的机会
+     */
+    public void createMap() {
+        //1000次画图机会
+        for (int i = 0; i < 1000; i++) {
+            if (draw()) {
+                break;
+            }
+        }
+    }
+
+}
